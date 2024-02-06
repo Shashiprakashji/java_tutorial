a39f1b848ec6014689627a6ea0f517423d384b8e
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-02-06 23:46:56 +0530

    another commit done

--- src/Item.java
+++ src/Item.java
@@ -1,31 +1,33 @@
 public class Item {
+
     private String type;
     private String name;
-
     private double price;
-    private String size="Medium";
+    private String size = "MEDIUM";
 
-    public Item(String type,String name,  double price) {
+    public Item(String type, String name, double price) {
         this.type = type.toUpperCase();
         this.name = name.toUpperCase();
-
         this.price = price;
     }
 
     public String getName() {
-        if(type.equals("SIDE")||type.equals("DRINK")){
-            return size+" "+name;
+
+        if (type.equals("SIDE") || type.equals("DRINK")) {
+            return size + " " + name;
         }
+
         return name;
     }
-    public double getBasePrice(){
+
+    public double getBasePrice() {
         return price;
     }
 
     public double getAdjustedPrice() {
-        return switch (size){
-            case "SMALL"-> getBasePrice()-0.5;
-            case "LARGE"-> getBasePrice()+1;
+        return switch (size) {
+            case "SMALL" -> getBasePrice() - 0.5;
+            case "LARGE" -> getBasePrice() + 1;
             default -> getBasePrice();
         };
     }
@@ -33,10 +35,13 @@ public class Item {
     public void setSize(String size) {
         this.size = size;
     }
-    public static void printItem(String name,double price){
-        System.out.printf("%20s:%6.2f%n",name,price);
+
+    public static void printItem(String name, double price) {
+        System.out.printf("%20s:%6.2f%n", name, price);
     }
-    public void printItem(){
-        printItem(getName(),getAdjustedPrice());
+
+    public void printItem() {
+        printItem(getName(), getAdjustedPrice());
     }
+
 }
