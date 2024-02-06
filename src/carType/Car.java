3149858d1b9c4d573fa455c4b24ac59de9aa0ba9
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-02-06 20:20:55 +0530

    second commit done

--- src/carType/Car.java
+++ src/carType/Car.java
@@ -16,7 +16,40 @@ public class Car {
     protected void runEngine(){
         System.out.println("car: run engine");
     }
-
 }
+class GasPoweredCar extends Car{
+    private double avgKMPerLitre;
+    private int cylinders=7;
+
+    public GasPoweredCar(String description) {
+        super(description);
+    }
+
+    public GasPoweredCar(String description, double avgKMPerLitre, int cylinders) {
+        super(description);
+        this.avgKMPerLitre = avgKMPerLitre;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Gas: all %d cylinders are fired up, Ready %n",cylinders);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Gas: usage exceeds the average: %.2f %n",avgKMPerLitre);
+    }
+}
+//    class ElectricCar extends Car{
+//        private double avgKMPerCharge;
+//        private  int batterySize;
+//    }
+//    class HybridCar extends Car{
+//        private double avgKMPerLitre;
+//        private  int batterySize;
+//        private int cylinders;
+//
+//    }
 
 
--- src/carType/Main.java
+++ src/carType/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Car car=new Car("Tata Nexon");
         runRace(car);
+
+        Car ferrari=new GasPoweredCar("GTA Ferrari",15.4,6 );
+        runRace(ferrari);
+
     }
     public  static void runRace(Car car){
         car.startEngine();
